8ecb24b270cb74e9488db54051d96b8991793046
Mark Lowel Montealto <lowel.montealto@whitecloak.com>
2025-09-26 14:49:32 +0800

    Todo: Rate Limit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
     const payload = { name: user.username, email: user.email};
     return {
       status: 200,
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, {secret: 'test123'}),
     };
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto'
 import { EncryptionService } from 'src/encryption.service';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
-
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService, private encryptionService: EncryptionService) {}
+  constructor(private readonly usersService: UsersService, private encryptionService: EncryptionService) { }
+
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto.password);
     this.encryptionService.encrpytText(createUserDto.password).then(hashedPassword => {
       createUserDto.password = hashedPassword;
+      console.log(createUserDto.password)
       return this.usersService.createUser(createUserDto);
     });
+
   }
 
 
@@ -23,7 +27,7 @@ export class UsersController {
     return this.usersService.users({});
   }
 
-  
+
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.deleteUser(+id);
